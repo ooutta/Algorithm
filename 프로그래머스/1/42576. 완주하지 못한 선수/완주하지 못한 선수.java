e9935fac58e256b16d4a31e16f8bef1c22581eb9
KimSuhyun <93786956+ooutta@users.noreply.github.com>
2023-10-19 21:14:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 193.27 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        // String[] t = {"mislav", "stanko", "mislav", "ana", "mislav"};
+        // String [] tt = {"stanko", "ana", "mislav"};
+        
+        HashSet<String> setMap = new HashSet<>();
+        List<String> duplicateNameList = new ArrayList<>();
+        
+        for(String name : participant) {
+            if(!setMap.contains(name))
+                setMap.add(name);
+            else 
+                duplicateNameList.add(name);
+        }
+        for(String name : completion) {
+            if(setMap.contains(name)) {
+                setMap.remove(name);
+            } else if(duplicateNameList.contains(name)) {
+                duplicateNameList.remove(name);
+            }
+        }
+        
+        if(setMap.size() > 0) {
+            Iterator<String> iter = setMap.iterator();
+            return iter.next();
+        } 
+        else {
+            Iterator<String> iter = duplicateNameList.iterator();
+            return iter.next();
+        }
+    }
+}
